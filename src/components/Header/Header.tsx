6298b4e341f71250832ef4f6e48106b5bd73553e
Harjot <singh.harjot85@gmail.com>
2025-11-25 19:12:52 -0800

    Fix Header items justification

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,9 @@
 function Header() {
     return (
-        <div className="bg-base-100">
-            <div className="container mx-auto mt-10 py-10 bg-secondary">
-                <div className="flex justify-center">
-                    <div className="text-2xl font-bold text-accent-content">
-                        AI WOG
-                    </div>
+        <div className="container  mt-10 py-10 bg-primary">
+            <div className="flex justify-center">
+                <div className="text-2xl font-bold text-accent-content">
+                    AI WOG
                 </div>
             </div>
         </div>
