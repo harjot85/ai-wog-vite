51063e4335bbadf26943ffc1c0de1867072dddcc
Harjot <singh.harjot85@gmail.com>
2025-11-25 18:37:59 -0800

    Add First component - Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
     return (
         <>
-            <h1 className="text-3xl font-bold underline text-amber-300">
-                AI Powered WOG
-            </h1>
+            <Home />
         </>
     );
 }
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+function Header() {
+    return (
+        <div className="bg-base-100">
+            <div className="container mx-auto mt-10 py-10 bg-secondary">
+                <div className="flex justify-center">
+                    <div className="text-2xl font-bold text-accent-content">
+                        AI WOG
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
