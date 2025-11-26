e8b44ae95152277b54179d84924ede576606eb37
Harjot <singh.harjot85@gmail.com>
2025-11-25 17:44:20 -0800

    First Prettier format

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
 
 function App() {
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline text-amber-300">AI WOG</h1>
-    </>
-  );
+    return (
+        <>
+            <h1 className="text-3xl font-bold underline text-amber-300">
+                AI WOG
+            </h1>
+        </>
+    );
 }
 
 export default App;
