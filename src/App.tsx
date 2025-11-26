abbac7c42e461d91f2d00344448a0c47f6feda9a
Harjot <singh.harjot85@gmail.com>
2025-11-25 17:54:18 -0800

    Setup Husky and Lint-Staged

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
     return (
         <>
             <h1 className="text-3xl font-bold underline text-amber-300">
-                AI WOG
+                AI Powered WOG
             </h1>
         </>
     );
